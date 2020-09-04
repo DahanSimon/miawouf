cfbbd895bcc988ef2e233dc62b68a4cdde59d583
DahanSimon <dahan.simon@hotmail.fr>
2020-09-04 11:40:21 +0200

    Cannot use custom class in storyboard

---
+++ miawouf/Controller/WelcomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeViewController.swift
+//  miawouf
+//
+//  Created by Simon Dahan on 04/09/2020.
+//  Copyright © 2020 Simon Dahan. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
