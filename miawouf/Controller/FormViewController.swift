5adfe04ec9ad6ac960d8d1e5d73819bc939ecf53
DahanSimon <dahan.simon@hotmail.fr>
2020-09-04 15:09:16 +0200

    SuccesViewController and FormViewController added

---
+++ miawouf/Controller/FormViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FormViewController.swift
+//  miawouf
+//
+//  Created by Simon Dahan on 04/09/2020.
+//  Copyright © 2020 Simon Dahan. All rights reserved.
+//
+
+import UIKit
+
+class FormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
